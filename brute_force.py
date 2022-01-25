31b6684578858691f7bfdccafba42bebcc771b0c
DanielNikkari <daniel.nikkari@gmail.com>
2022-01-25 18:56:17 +0200

    Created succesfull demo

--- brute_force.py
+++ brute_force.py
@@ -25,10 +25,10 @@ guess = ""
 
 
 def bruteforce(charset, maxlength):
-    n = 9
+    # n = 9
     return (''.join(candidate)
             for candidate in chain.from_iterable(product(charset, repeat=i)
-                                                 for i in range(n, maxlength + 1)))
+                                                 for i in range(1, maxlength + 1)))
 
 
 def main():
@@ -38,7 +38,7 @@ def main():
     # print(string.ascii_uppercase)
 
     # Max length for the brute force
-    maxlength = 10
+    maxlength = 3
 
     # Number of iterations
     n = 0
@@ -50,28 +50,30 @@ def main():
     print("\n!-------------------------------------------------!\n")
     print("Starting the loop\n")
 
-    test_hash = hashlib.sha1(bytes('000001mu', 'ascii')).hexdigest()
+    test_hash = hashlib.sha1(bytes('abc', 'ascii')).hexdigest()
     print("Test hash: \n{}\n\n".format(test_hash))
 
     # Looping attempts and testing if it matches the given hash
     for attempt in bruteforce(ascii_charset, maxlength):
-        #stdout.write("\rAttempts: {}, Attempt: {}".format(n, attempt))
-        # stdout.flush()
+        stdout.write("\r%d attempts" % n)
+        stdout.flush()
 
         # Transform the attempt into a hash
         attempt_hash = hashlib.sha1(bytes(attempt, 'ascii')).hexdigest()
         if attempt_hash == test_hash:
-            print("Password {} found after {} tries".format(attempt, n))
+            print("\n\n!------------------------------------------------------------------!\n")
+            print("\n\tPassword: {}, found after {} attempts.\n\n".format(attempt, n))
+            print("!------------------------------------------------------------------!\n")
             brute_forced_password = attempt
             brute_forced_password_hash = attempt_hash
             break
         else:
             n = n + 1
             continue
-    # stdout.write("\n")
+    stdout.write("\n")
 
     print("Found hash: {}\nThe given hash: {}".format(
-        attempt_hash, password_hash))
+        attempt_hash, test_hash))
 
 
 if __name__ == "__main__":
